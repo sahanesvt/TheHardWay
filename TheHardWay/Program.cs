5c290e6310018732ed9f7bce238bd0ffbd893633
sahan <sahan@DESKTOP-PP8MJTV>
2017-11-18 23:00:42 -0500

    First

--- TheHardWay/Program.cs
+++ TheHardWay/Program.cs
@@ -10,6 +10,49 @@ namespace TheHardWay
     {
         static void Main(string[] args)
         {
+            //Calculate the area and perimeter of three different rectangles... the hard way
+            double width1, depth1, area1, perimeter1, width2, depth2, area2, perimeter2, width3, depth3, area3, perimeter3;
+
+            width1 = 1;
+            depth1 = 2;
+            area1 = width1 * depth1;                                //bad way to do repetitive operation
+            perimeter1 = 2 * (width1 + depth1);                     //bad way to do repetitive operation
+            area1 = calcArea(width1, depth1);                       //slightly better way to do repetitive operation, but we still end up repeating ourselves when we set properties manually
+                                                                    //should create "Rectangle" class and write code only once for how to calculate properties 
+            perimeter1 = calcPerimeter(width1, depth1);
+            
+            width2 = 3;
+            depth2 = 4;
+            area2 = width2 * depth2;                                //bad way to do repetitive operation
+            perimeter2 = 2 * (width2 + depth2);                     //bad way to do repetitive operation
+            area2 = calcArea(width2, depth2);                       //slightly better way to do repetitive operation, but we still end up repeating ourselves when we set properties manually
+                                                                    //should create "Rectangle" class and write code only once for how to calculate properties               
+            perimeter2 = calcPerimeter(width2, depth2);
+
+            width3 = 5;
+            depth3 = 6;
+            area3 = width3 * depth3;                                //bad way to do repetitive operation
+            perimeter3 = 2 * (width3 + depth3);                     //bad way to do repetitive operation
+            area3 = calcArea(width3, depth3);                       //slightly better way to do repetitive operation, but we still end up repeating ourselves when we set properties manually
+                                                                    //should create "Rectangle" class and write code only once for how to calculate properties
+            perimeter3 = calcPerimeter(width3, depth3);
+
+            Rectangle rectangle1 = new Rectangle(1, 2);             //much better way to define a rectangle, now if we need to access the different properties of rectangle1,  
+            Rectangle rectangle2 = new Rectangle(3, 4);             //we would simply use rectangle1.Width or rectangle1.Depth or rectangle1.Area or rectangle1.Perimeter
+            Rectangle rectangle3 = new Rectangle(5, 6);
+
+
+
+        }
+
+        static double calcArea(double width, double depth)
+        {
+            return width * depth;
+        }
+
+        static double calcPerimeter(double width, double depth)
+        {
+            return 2 * (width + depth);
         }
     }
 }
---
+++ TheHardWay/Rectangle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheHardWay
+{
+    public class Rectangle
+    {
+        public double Width;
+        public double Depth;
+        public double Area;
+        public double Perimeter;
+
+        public Rectangle(double width, double depth)            //defines how we create an instance of a Rectangle object, and what (if any) parameters are required.
+        {
+            Width = width;
+            Depth = depth;
+            calcArea();                                         //ok to use private method here since we're inside the class
+            Area = returnArea();                                //another way we could set the "Area" property
+            Area = returnArea(Width, Depth);                    //another way we could set the "Area" property
+            calcPerimeter();
+        }
+
+        public Rectangle()
+        {
+            Width = 0;
+            Depth = 0;
+            calcArea();
+            calcPerimeter();
+        }
+
+
+        private void calcArea()                                 //Set as private so can only be used inside the Rectangle class, and not outside
+        {                                                       //Outside the class, Rectangle.Area has meaning, but Rectangle.calcArea() is invalid since it is private                        
+            Area = Width * Depth;                               //Note that this method is "void" meaning it doesn't return any value, but rather performs an operation,
+        }                                                       //which in this case is defining the value of "Area"            
+
+        private double returnArea()                             //for illustrative purposes, we could accomplish the same thing with a method that returns the value of "Area"
+        {
+            return Width * Depth;
+        }
+
+        private double returnArea(double width, double depth)   //yet another way we could accomplish the same goal, note that "width" and "depth" are not the same as
+        {                                                       //"Width" and "Depth".  Here they are variable local to the "returnArea()" method and cannot be accessed outside the method        
+            return width * depth;                               //Also note that this method as the same name as the previous method,             
+        }                                                       //this is called "overloading" and is allowed so long as parameters are different
+
+        private void calcPerimeter()
+        {
+            Perimeter = 2 * (Width + Depth);
+        }
+    }
+}
